5d2612335028dac82240d8cb4c9310c102f3cd4a
habib-sc <habib97.cs@gmail.com>
2022-02-12 18:36:04 +0600

    comment out login.js file

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
+// Login Input function 
 function getLoginInput(emailFieldId, passwordFieldId) {
+    // Getting Input 
     const emailField = document.getElementById(emailFieldId);
     const passwordField = document.getElementById(passwordFieldId)
 
@@ -6,14 +8,18 @@ function getLoginInput(emailFieldId, passwordFieldId) {
     const email = emailField.value;
     const password = passwordField.value;
 
+    // Returning value 
     return [email, password];
 }
 
+// Login Handler 
 document.getElementById('login-btn').addEventListener('click', function() {
+    // Getting Input value 
     const loginInput = getLoginInput('email-field', 'password-field');
     const email = loginInput[0];
     const password = loginInput[1];
 
+    // Checking email and password they matching or not 
     if (email == 'customer@gmail.com' && password == 'password') {
         window.location.href = 'banking.html';
     }
